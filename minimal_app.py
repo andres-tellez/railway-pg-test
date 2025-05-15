2328455b019e692997a6ef1c729174ecbea3827b
Andres <andrestellez@outlook.com>
2025-05-14 23:12:50 -0500

    dewqd

--- minimal_app.py
+++ minimal_app.py
@@ -1,18 +1,16 @@
-# smoke_test.py
-
 import os
 from flask import Flask
 
-smoke = Flask(__name__)
+app = Flask(__name__)
 
-@smoke.route("/")
+@app.route("/")
 def home():
     return "🚂 Smoke test v3!"
 
-@smoke.route("/test-connect")
+@app.route("/test-connect")
 def test_connect():
     return "Test endpoint v3 OK", 200
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
-    smoke.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
