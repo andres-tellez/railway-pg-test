c0c5a677f14f261846d39226e0e0b960b2833948
Andres <andrestellez@outlook.com>
2025-05-28 20:54:05 -0500

    Add Activity model for DAO sync

---
+++ src/db/models/activities.py
@@ -0,0 +1,22 @@
+# src/db/models/activities.py
+
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Activity(Base):
+    __tablename__ = "activities"
+
+    activity_id = Column(Integer, primary_key=True, index=True)
+    athlete_id = Column(Integer, nullable=False, index=True)
+
+    name = Column(String)
+    type = Column(String)
+    start_date = Column(DateTime)
+    distance = Column(Float)
+    elapsed_time = Column(Integer)
+    moving_time = Column(Integer)
+    total_elevation_gain = Column(Float)
+    external_id = Column(String)
+    timezone = Column(String)
