cbe44e3d58712874475b28f335e9e26364212e0d
Andres <andrestellez@outlook.com>
2025-06-23 21:05:53 -0500

    ✅ Moved sample activity data to utils for production safety

--- src/utils/seeder.py
+++ src/utils/seeder.py
@@ -1,7 +1,8 @@
 # src/utils/seeder.py
 
 from src.db.dao.activity_dao import ActivityDAO
-from tests.test_data.sample_activities import SAMPLE_ACTIVITY_JSON
+from src.utils.sample_data import SAMPLE_ACTIVITY_JSON
+
 
 def seed_sample_activity(session, athlete_id: int):
     mock = SAMPLE_ACTIVITY_JSON.copy()
