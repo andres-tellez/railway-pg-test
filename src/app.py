63c524add964dd7fbffee500ef32937a4290b5b2
Andres <andrestellez@outlook.com>
2025-07-06 20:29:44 -0500

    fix: correct static path for Railway production

--- src/app.py
+++ src/app.py
@@ -46,7 +46,8 @@ def create_app(test_config=None):
     BASE_DIR = Path(__file__).resolve().parent.parent
     #app = Flask(__name__, static_folder=str(BASE_DIR / "static"), static_url_path="/")
     
-    app = Flask(__name__, static_folder="static", static_url_path="/") 
+    app = Flask(__name__, static_folder="../static", static_url_path="/")
+
 
 
 
