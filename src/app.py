28ffff5ab7366a3a65a1e766f3707fb2c4127ee7
Andres <andrestellez@outlook.com>
2025-08-18 04:48:43 -0500

    testing4

--- src/app.py
+++ src/app.py
@@ -101,7 +101,7 @@ def create_app(test_config=None):
         SESSION_COOKIE_SAMESITE="None",
         SESSION_COOKIE_SECURE=True,
         SESSION_COOKIE_HTTPONLY=True,
-        SESSION_COOKIE_DOMAIN=".smartcoach.dev",
+        SESSION_COOKIE_DOMAIN="smartcoach.dev",
     )
 
     app.config.from_mapping(
